09907061cce98b3af81f7b87a0f8c0026c3b0dfc
Nat14 <naryucha14@gmail.com>
2016-03-06 21:37:45 -0800

    updated hilo

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,7 @@
 class GameController < ApplicationController
+
   def play_games
+    ### magic 8 game
     # added answer to array
     answer_array = ["Yup!","Fuhgeddaboudit", "Maybe", "Ask: what would your mother do?", "Ask: what would an Australian do, then do the opposite", "Your answer here"]
 
@@ -7,8 +9,36 @@ class GameController < ApplicationController
     if params.has_key?(:q1) && !params[:q1].strip.empty?
       num = Random.new
       a = num.rand(answer_array.length)
-      flash.now[:alert] = answer_array[a]
+      flash.now[:message] = answer_array[a]
       @question = params[:q1]+"?" if params[:q1][-1] != "?"
     end
   end
+
+  def play_hilo
+    ### hilo game
+    if cookies[:secret_number].nil?
+        cookies[:secret_number] = Random.rand(100)
+        cookies[:all_guess] = " "
+    end
+
+    @secret_number = cookies[:secret_number].to_i
+    @guess = params[:guess].to_i
+
+    if @guess < 100 && @guess > 0
+      cookies[:all_guess] = cookies[:all_guess] + @guess.to_s + ", "
+      if @secret_number == @guess
+        flash.now[:notice] = "You win! #{@guess} is correct number. Guess another number to play again!"
+        cookies[:secret_number] = Random.rand(100)
+        cookies[:all_guess] = " "
+      elsif @secret_number > @guess
+        flash.now[:alert] = "Too Low, guess again!"
+      elsif @secret_number < @guess
+        flash.now[:alert] = "Too High, guess again!"
+      end
+    else
+      flash.now[:alert] = "Please enter a NUMBER between 1 and 100!"
+    end
+    redirect_to '/game/play_games#set-2'
+  end
+
 end
